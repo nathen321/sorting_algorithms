44e6068da8d9d33c1f5fc8898bdde02be2ad4918
nathen321 <walidjalaly43@gmail.com>
2024-02-20 09:52:09 -0800

    Insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - ....
+ *
+ * @list: ...
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while (node->prev && node->prev->n > node->n)
+		{
+			node = swap_node(list, node);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+
+/**
+ * insertion_sort_list - ....
+ *
+ * @list: ...
+ * @node: ...
+ *
+ * Return: void
+ */
+listint_t *swap_node(listint_t **list, listint_t *node)
+{
+	listint_t *back = node->prev;
+	listint_t *idx = node;
+
+	if (back->prev)
+		back->prev->next = idx;
+	else
+		*list = idx;
+	if (idx->next)
+		idx->next->prev = back;
+	idx->prev = back->prev;
+	back->next = idx->next;
+	idx->next = back;
+	back->prev = idx;
+	return (idx);
+}
