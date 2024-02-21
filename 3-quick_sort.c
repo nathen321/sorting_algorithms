3a4799fc51fc0b195ffc366478f007e932667041
nathen321 <walidjalaly43@gmail.com>
2024-02-20 17:42:48 -0800

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ *quick_sort - prepare the terrain to quicksort algorithm
+ *@array: array
+ *@size: array size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
